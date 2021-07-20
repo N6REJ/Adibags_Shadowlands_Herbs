47c622a25604789974ddba727640d7c79f97a8fa
Bear <programming@hallhome.us>
2021-07-19 19:04:16 -0500

    Add files via upload

---
+++ database.lua
@@ -0,0 +1,23 @@
+-- AdiBags_Shadowlands_Crafting - herbs Database
+-- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+--
+local ADDON_NAME, ADDON_TABLE, addon = ...
+
+-- Create addon table
+N = { }
+ADDON_TABLE.N = N
+
+-- Label to use
+N["FilterTitle"] = "herbs"
+
+-- Database of items
+N["database"] = {
+	-- ID,	--Item name
+	168583,		-- Widowbloom
+	168586,		-- Rising Glory
+	168589,		-- Marrow Root
+	169701,		-- Death Blossom
+	170554,		-- Vigils Torch
+	171315,		-- Nightshade
+}
