02f71b0f99aa972af34c4b8022cd17262f631a5a
N6REJ <programming@hallhome.us>
2021-11-04 09:27:46 -0500

    updated core files to "unified" file system

--- database.lua
+++ database.lua
@@ -1,23 +1,32 @@
--- AdiBags_Shadowlands_Crafting - herbs Database
+-- AdiBags_Shadowlands Herbs - Database
 -- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
--- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+-- Source on GitHub: https://n6rej.github.io
 --
-local ADDON_NAME, ADDON_TABLE, addon = ...
+local addonName, addonTable, addon = ...
 
 -- Create addon table
-N = { }
-ADDON_TABLE.N = N
+local db = { }
 
--- Label to use
-N["FilterTitle"] = "herbs"
+db.name = "Shadowlands Fishing"
+db.desc = "Fish and Bait found while fishing in shadowlands"
 
--- Database of items
-N["database"] = {
-	-- ID,	--Item name
-	168583,		-- Widowbloom
-	168586,		-- Rising Glory
-	168589,		-- Marrow Root
-	169701,		-- Death Blossom
-	170554,		-- Vigils Torch
-	171315,		-- Nightshade
+-- Filter info
+db.Filters = {
+	["Herbs"] = {
+		uiName = "Shadowlands Herbs",
+		uiDesc = "Herbs found while gathering in Shadowlands",
+		title = "Herbs",
+		items = {
+			-- ID,	--Item name
+			[168583] = true, 		-- Widowbloom
+			[168586] = true, 		-- Rising Glory
+			[168589] = true, 		-- Marrow Root
+			[169701] = true, 		-- Death Blossom
+			[170554] = true, 		-- Vigils Torch
+			[171315] = true, 		-- Nightshade
+		},
+	},
 }
+
+-- now that db is populated lets pass it on.
+addonTable.db = db
